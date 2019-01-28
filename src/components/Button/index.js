6c5d28229964edd3cc8abf8ae723e91439e6df02
Matthew Wells <wellsmattd@gmail.com>
2019-01-28 11:19:01 -0600

    Adds basic Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import styles from './styles.css';
+
+export default props => (
+  <button {...props} className={styles.Button} />
+);
--- src/index.js
+++ src/index.js
@@ -1,22 +1,5 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import Button from './components/Button';
 
-import styles from './styles.css'
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
-
-  render() {
-    const {
-      text
-    } = this.props
-
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
-}
+export default {
+  Button,
+};
