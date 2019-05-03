7c9720cf1a85dd593176223305423561415e3f5d
Matthew Wells <wellsmattd@gmail.com>
2019-05-03 16:02:24 -0500

    Updates BarStat and integrate into storybook

--- src/components/BarStat/index.js
+++ src/components/BarStat/index.js
@@ -1,50 +1,64 @@
-import React, { Component } from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { color, fontFamily, fontSize, height, width } from 'styled-system';
+import withDefaultTheme from '../withDefaultTheme';
 
-import styles from './style.css';
+export const Label = withDefaultTheme(styled.span(fontFamily, fontSize));
+Label.defaultProps = {
+  fontFamily: 'body',
+  fontSize: 1,
+};
 
-export default class BarStat extends Component {
-  static propTypes = {
-    percent: PropTypes.number.isRequired,
-    showLabel: PropTypes.bool,
-  };
+export const Bar = withDefaultTheme(
+  styled.div(
+    {
+      transition: 'width 1s ease',
+      display: 'flex',
+      boxSizing: 'border-box',
+    },
+    color,
+    height,
+    width
+  )
+);
 
-  static defaultProps = {
-    showLabel: false,
-  };
+Bar.displayName = 'Bar';
+Bar.defaultProps = {
+  bg: 'grayLite.4',
+  width: '100%',
+  height: '10px',
+};
 
-  state = {
-    show: false,
-  };
+const BarStat = props => {
+  const { percent } = props;
+  const percentString = `${percent}%`;
+  let widthPercent = 0;
 
-  componentDidMount() {
-    if (!this.state.show) {
-      requestAnimationFrame(() => this.setState({ show: true })); // eslint-disable-line no-undef
-    }
+  if (percent > 0 && percent < 1) {
+    widthPercent = '1px';
+  } else {
+    widthPercent = percentString;
   }
 
-  render() {
-    const { percent, showLabel } = this.props;
-    const { show } = this.state;
-    const percentString = `${percent}%`;
-    let widthPercent = '0px';
-    if (show) {
-      if (percent > 0 && percent < 1) {
-        widthPercent = '1px';
-      } else {
-        widthPercent = percentString;
-      }
-    }
+  return (
+    <Fragment>
+      <Bar>
+        <Bar bg="grayLite.1" width={widthPercent} />
+      </Bar>
+      {props.children}
+    </Fragment>
+  );
+};
 
-    return (
-      <div className={styles.BarStat}>
-        <div className={styles.bar}>
-          <div className={styles.fill} style={{ width: `${widthPercent}` }} />
-        </div>
-        {showLabel && (
-          <div className={styles.label}>{parseInt(percent, 10)}%</div>
-        )}
-      </div>
-    );
-  }
-}
+BarStat.propTypes = {
+  percent: PropTypes.number,
+  children: PropTypes.node,
+};
+
+BarStat.defaultProps = {
+  percent: 0,
+  children: null,
+};
+
+export default BarStat;
---
+++ src/components/BarStat/story.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { number } from '@storybook/addon-knobs';
+import styled from 'styled-components';
+import { margin, space } from 'styled-system';
+
+// import BarStat from './';
+import BarStat, * as Bar from './index.js';
+import Paper from '../Paper';
+import BarStatReadme from './README.md';
+import withDefaultTheme from '../withDefaultTheme/index.js';
+
+const Wrap = withDefaultTheme(styled.div(margin));
+Wrap.defaultProps = { m: 3 };
+Wrap.displayName = 'Wrap';
+
+const Spacer = withDefaultTheme(styled.div(space));
+Spacer.defaultProps = { mb: 3 };
+Spacer.displayName = 'Spacer';
+
+const options = {
+  range: true,
+  min: 0,
+  max: 100,
+  step: 1,
+};
+
+storiesOf('Solar System/BarStat', module)
+  .addParameters({
+    readme: {
+      sidebar: BarStatReadme,
+    },
+  })
+  .add('Overview', () => {
+    const value = number('Percent', 73, options);
+    return (
+      <Wrap>
+        <BarStat percent={value}>
+          <Bar.Label>Percent: {value}</Bar.Label>
+        </BarStat>
+      </Wrap>
+    );
+  })
+  .add('<Bar>', () => (
+    <Wrap>
+      <Paper mb={2}>
+        <h3>Default Bar</h3>
+        <Bar.Bar />
+      </Paper>
+
+      <Paper mb={2}>
+        <h3>Brand Color Bar</h3>
+        <Bar.Bar bg="brandColor" />
+      </Paper>
+
+      <Paper mb={2}>
+        <h3>Bar with custom background and width</h3>
+        <Bar.Bar bg="green" width="25%" />
+        <Spacer />
+        <Bar.Bar bg="green" width="50%" />
+      </Paper>
+
+      <Paper mb={2}>
+        <h3>Bars can be nested (BarStat uses this)</h3>
+        <Bar.Bar width="100%">
+          <Bar.Bar bg="green" width="25%" />
+          <Bar.Bar bg="brandColor" width="10%" />
+        </Bar.Bar>
+      </Paper>
+    </Wrap>
+  ))
+  .add(
+    'Examples',
+    () => (
+      <div>
+        {[null, 0, 10, 20, 50, 99, 100].map(percent => (
+          <Wrap key={`${percent}`.toString()}>
+            <BarStat percent={percent}>
+              <Bar.Label>Percent: {percent || 0}%</Bar.Label>
+            </BarStat>
+          </Wrap>
+        ))}
+      </div>
+    ),
+    {
+      info: {
+        inline: true,
+        source: true,
+      },
+    }
+  );
