b17f86b57f37eef2bbb15c9fb8b4827d98d5d9a1
Matthew Wells <wellsmattd@gmail.com>
2019-06-14 11:31:43 -0500

    Adds basic tests for Paper component

---
+++ src/components/Paper/test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+
+import Paper from './';
+
+describe('Paper', () => {
+  it('renders', () => {
+    shallow(<Paper>Boom</Paper>);
+  });
+
+  describe('with children renders children', () => {
+    it(`renders it's children`, () => {
+      const wrapper = mount(
+        <Paper>
+          <div>Hello</div>
+        </Paper>
+      );
+
+      expect(wrapper.children()).toHaveLength(1);
+      expect(wrapper.childAt(0).text()).toEqual('Hello');
+    });
+
+    it(`renders with default props`, () => {
+      const wrapper = mount(
+        <Paper>
+          <div>Hello</div>
+        </Paper>
+      );
+      expect(wrapper.props().bg).toEqual('white');
+      expect(wrapper.props().borderRadius).toEqual('2px');
+      expect(wrapper.props().color).toEqual('gray');
+    });
+
+    it(`renders with style prop overrides`, () => {
+      const wrapper = mount(
+        <Paper p={6}>
+          <div>Hello</div>
+        </Paper>
+      );
+      expect(wrapper.props().p).toEqual(6);
+    });
+  });
+});
