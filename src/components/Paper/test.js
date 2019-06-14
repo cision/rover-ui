b3bf26ecad8faf57e3e7dddde381849baa2526ee
Matthew Wells <wellsmattd@gmail.com>
2019-06-14 15:12:00 -0500

    Simplify Paper tests. Not complex enough to warrant much more

--- src/components/Paper/test.js
+++ src/components/Paper/test.js
@@ -17,27 +17,7 @@ describe('Paper', () => {
       );
 
       expect(wrapper.children()).toHaveLength(1);
-      expect(wrapper.childAt(0).text()).toEqual('Hello');
-    });
-
-    it(`renders with default props`, () => {
-      const wrapper = mount(
-        <Paper>
-          <div>Hello</div>
-        </Paper>
-      );
-      expect(wrapper.props().bg).toEqual('white');
-      expect(wrapper.props().borderRadius).toEqual('2px');
-      expect(wrapper.props().color).toEqual('gray');
-    });
-
-    it(`renders with style prop overrides`, () => {
-      const wrapper = mount(
-        <Paper p={6}>
-          <div>Hello</div>
-        </Paper>
-      );
-      expect(wrapper.props().p).toEqual(6);
+      expect(wrapper.text()).toEqual('Hello');
     });
   });
 });
