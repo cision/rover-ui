7467ecbf7f9cc7611b5904473e35f1126a5c8a63
Matthew Wells <mdespuits@users.noreply.github.com>
2019-06-17 12:07:00 -0500

    Adds basic tests for Paper component (#52)
    * Adds basic tests for Paper component
    
    * grammar nerd fix
    
    * Simplify Paper tests. Not complex enough to warrant much more

---
+++ src/components/Paper/test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+
+import Paper from './';
+
+describe('Paper', () => {
+  it('renders', () => {
+    shallow(<Paper>Boom</Paper>);
+  });
+
+  describe('with children renders children', () => {
+    it('renders its children', () => {
+      const wrapper = mount(
+        <Paper>
+          <div>Hello</div>
+        </Paper>
+      );
+
+      expect(wrapper.children()).toHaveLength(1);
+      expect(wrapper.text()).toEqual('Hello');
+    });
+  });
+});
