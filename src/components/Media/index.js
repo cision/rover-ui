3e3b24049fa7148a9c91683b8d51c89abbabdbaf
Matthew Wells <wellsmattd@gmail.com>
2019-06-24 13:45:31 -0500

    Cleanup organization of Media object pieces

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -1,46 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import { space } from 'styled-system';
-import { propTypes as TagType } from '../../shared/models/tag';
-import withDefaultTheme from '../withDefaultTheme';
+import { StyledMedia, StyledBody, StyledItem } from './styled.js';
 
-const BaseMediaObj = ({ tag: Tag, ...props }) => <Tag {...props} />;
-BaseMediaObj.propTypes = {
-  tag: TagType,
-};
-
-BaseMediaObj.defaultProps = {
-  tag: 'div',
-};
-
-const StyledMedia = styled(BaseMediaObj)`
-  display: flex;
-  flex-flow: row nowrap;
-  align-items: stretch;
-  width: 100%;
-  ${space}
-`;
-
-const Media = withDefaultTheme(StyledMedia);
+const Media = StyledMedia;
 Media.displayName = 'Media';
 
-const StyledBody = styled(BaseMediaObj)`
-  flex: 1 1 auto;
-  min-width: 0;
-  display: grid;
-  grid-template-columns: 100%;
-  ${space}
-`;
-const Body = withDefaultTheme(StyledBody);
+const Body = StyledBody;
 Body.displayName = 'Media.Body';
 
-const StyledItem = styled(BaseMediaObj)`
-  flex: 0 0 auto;
-  display: grid;
-  grid-template-columns: 100%;
-  ${space}
-`;
-const Item = withDefaultTheme(StyledItem);
+const Item = StyledItem;
 Item.displayName = 'Media.Item';
 
 Media.Body = Body;
---
+++ src/components/Media/styled.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from 'styled-components';
+import { space } from 'styled-system';
+import { propTypes as TagType } from '../../shared/models/tag';
+import withDefaultTheme from '../withDefaultTheme';
+
+const BaseMediaObj = ({ tag: Tag, ...props }) => <Tag {...props} />;
+BaseMediaObj.propTypes = {
+  tag: TagType,
+};
+
+BaseMediaObj.defaultProps = {
+  tag: 'div',
+};
+
+export const StyledMedia = withDefaultTheme(styled(BaseMediaObj)`
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: stretch;
+  width: 100%;
+  ${space}
+`);
+
+export const StyledBody = withDefaultTheme(styled(BaseMediaObj)`
+  flex: 1 1 auto;
+  min-width: 0;
+  display: grid;
+  grid-template-columns: 100%;
+  ${space}
+`);
+
+export const StyledItem = withDefaultTheme(styled(BaseMediaObj)`
+  flex: 0 0 auto;
+  display: grid;
+  grid-template-columns: 100%;
+  ${space}
+`);
