9f53c50835fd91ee39922206990734bb416d16f2
Matthew Wells <wellsmattd@gmail.com>
2019-06-24 16:05:53 -0500

    Adds an Overview to Media object storybook

--- src/components/Media/story.js
+++ src/components/Media/story.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { times } from 'lodash';
 import { storiesOf } from '@storybook/react';
+import { number, text, select } from '@storybook/addon-knobs';
 
 import Media from './';
 import Paper from '../Paper';
 import Readme from './README.md';
+import { spaceMap } from '../../shared/sizing';
 
 storiesOf('Dark Matter/Media', module)
   .addParameters({
@@ -11,6 +14,47 @@ storiesOf('Dark Matter/Media', module)
       sidebar: Readme,
     },
   })
+  .add(
+    'Overview',
+    () => {
+      const spacing = select('Spacing', spaceMap, 'xs');
+      const mediaTag = text('Media Tag', 'div');
+      const beforeCount = number('Items Before', 2);
+      const afterCount = number('Items After', 2);
+
+      const itemsBefore = times(beforeCount).map(idx => {
+        return (
+          <Media.Item key={idx}>
+            <Paper m={spacing}>{idx + 1}</Paper>
+          </Media.Item>
+        );
+      });
+
+      const itemsAfter = times(afterCount).map(idx => {
+        return (
+          <Media.Item key={idx}>
+            <Paper m={spacing}>{idx + 1}</Paper>
+          </Media.Item>
+        );
+      });
+
+      return (
+        <React.Fragment>
+          <Media tag={mediaTag}>
+            {itemsBefore}
+            <Media.Body>
+              <Paper m={spacing}>Body</Paper>
+            </Media.Body>
+            {itemsAfter}
+          </Media>
+        </React.Fragment>
+      );
+    },
+    {
+      inline: true,
+      source: true,
+    }
+  )
   .add(
     'Examples',
     () => (
