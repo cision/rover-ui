0e4b8f0a210fee8cfb01232bc4c58f8b0db184f3
Chris Garcia <pixelbandito@gmail.com>
2019-06-27 16:47:03 -0500

    Delete story from Grid.Entry (private use component)

--- src/components/Grid/Entry/story.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import Entry from './';
-
-storiesOf('Entry', module).add('Entry', () => (
-  <div>
-    <Entry />
-  </div>
-));
