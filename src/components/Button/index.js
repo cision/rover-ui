6aad159f4f1cda96db416052e0fe8ceae63d23f7
Chris Garcia <pixelbandito@gmail.com>
2019-06-28 14:32:00 -0500

    Move css imports to js instead of @imports as temp fix for bundling (#59)

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -6,6 +6,7 @@ import { propTypes as tagPropTypes } from '../../shared/models/tag';
 
 import Addon from './Addon';
 
+import './colors.css';
 import style from './style.css';
 
 const Button = props => {
