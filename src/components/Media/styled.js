33bdb6ce21e696aaa5e0274011951a5edabce594
Matthew Wells <wellsmattd@gmail.com>
2019-07-01 16:47:44 -0500

    Use flexbox correctly and just use divs for Media

--- src/components/Media/styled.js
+++ src/components/Media/styled.js
@@ -1,28 +1,20 @@
-import React from 'react';
 import styled from 'styled-components';
-import { space, flex } from 'styled-system';
-import { propTypes as TagType } from '../../shared/models/tag';
+import { space, flexbox } from 'styled-system';
 import withDefaultTheme from '../withDefaultTheme';
 
-const BaseMediaObj = ({ tag: Tag, ...props }) => <Tag {...props} />;
-BaseMediaObj.propTypes = {
-  tag: TagType,
-};
-
-BaseMediaObj.defaultProps = {
-  tag: 'div',
-};
-
-export const StyledMedia = withDefaultTheme(styled(BaseMediaObj)`
+export const StyledMedia = withDefaultTheme(styled.div`
   display: flex;
   flex-flow: row nowrap;
-  align-items: stretch;
   width: 100%;
-  ${flex}
+  ${flexbox}
   ${space}
 `);
 
-export const StyledBody = withDefaultTheme(styled(BaseMediaObj)`
+StyledMedia.defaultProps = {
+  alignItems: 'stretch',
+};
+
+export const StyledBody = withDefaultTheme(styled.div`
   flex: 1 1 auto;
   min-width: 0;
   display: grid;
@@ -30,7 +22,7 @@ export const StyledBody = withDefaultTheme(styled(BaseMediaObj)`
   ${space}
 `);
 
-export const StyledItem = withDefaultTheme(styled(BaseMediaObj)`
+export const StyledItem = withDefaultTheme(styled.div`
   flex: 0 0 auto;
   display: grid;
   grid-template-columns: 100%;
