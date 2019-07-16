1f8568f0bda6899175f8b480796fea86b6e5ccf2
Chris Garcia <pixelbandito@gmail.com>
2019-07-16 11:58:25 -0500

    Add babel and postcss plugins/config for IE 11, prefix css custom prop names (#69)

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
-import ExampleComponent from '@rover-ui';
+import { Badge, Button, Paper, Grid } from '@cision/rover-ui';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <ExampleComponent text="Modern React component module" />
+      <div style={{ background: 'gray' }}>
+        <Badge variant="info">Badge</Badge>
+        <Button level="secondary">Boom</Button>
+        <Grid columns={3} gutter="sm">
+          <Paper>Hi</Paper>
+          <Paper>Hi</Paper>
+          <Paper>Hi</Paper>
+          <Paper>Hi</Paper>
+          <Paper>Hi</Paper>
+          <Paper>Hi</Paper>
+          <Paper>Hi</Paper>
+        </Grid>
       </div>
     );
   }
