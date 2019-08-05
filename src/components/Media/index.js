1f4afbe8828eac48896f10aa7dd0519272ea0220
Matthew Wells <mdespuits@users.noreply.github.com>
2019-08-05 12:29:55 -0500

    Media uses CSS modules instead of styled-system (#83)
    * Media uses CSS modules instead of styled-system
    
    This way, CSS classes from the main application can override any default
    styles since styled-components injects CSS later in the load process.
    
    * Import without explicit filename
    
    * Media Body and Item both need min-width

--- src/components/Media/index.js
+++ src/components/Media/index.js
@@ -1,13 +1,41 @@
-import { StyledMedia, StyledBody, StyledItem } from './styled.js';
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import styled from 'styled-components';
+import { space, flex } from 'styled-system';
 
-const Media = StyledMedia;
-Media.displayName = 'Media';
+import styles from './style.css';
+import withDefaultTheme from '../withDefaultTheme';
 
-const Body = StyledBody;
-Body.displayName = 'Media.Body';
+const FlexSpacer = withDefaultTheme(styled.div(flex, space));
 
-const Item = StyledItem;
-Item.displayName = 'Media.Item';
+// Factory to build the components for the Media component
+// Since all three are very basic and use basically the same basis
+// underneath, we can construct these components programatically.
+const mediaFactory = (name, cssClass) => {
+  const MediaComponent = props => {
+    const newprops = {
+      ...props,
+      className: classNames(cssClass, props.className),
+    };
+    return <FlexSpacer {...newprops} />;
+  };
+
+  MediaComponent.propTypes = {
+    className: PropTypes.string,
+  };
+
+  MediaComponent.defaultProps = {
+    className: '',
+  };
+
+  MediaComponent.displayName = name;
+  return MediaComponent;
+};
+
+const Media = mediaFactory('Media', styles.Media);
+const Body = mediaFactory('Media.Body', styles.Body);
+const Item = mediaFactory('Media.Item', styles.Item);
 
 Media.Body = Body;
 Media.Item = Item;
