d24b4e59cb5dd1ec972d5f041689cfe6acc64c7f
Chris Garcia <pixelbandito@gmail.com>
2019-10-14 11:54:28 -0500

    Fixes 46 by adding unit tests for Button and Button.Addon (#116)
    * Fixes 46 by adding unit tests for Button and Button.Addon
    
    * Remove commented-out css module import

---
+++ src/components/Button/Addon/test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import Addon from './';
+
+describe('Addon', () => {
+  describe('Main component', () => {
+    it('renders an `Addon` tag without error', () => {
+      shallow(<Addon />);
+    });
+
+    describe('props.className', () => {
+      it('adds custom className', () => {
+        const wrapper = shallow(<Addon className="foo" />);
+        expect(wrapper.hasClass('foo')).toEqual(true);
+      });
+    });
+
+    describe('props.size', () => {
+      // Our current test build doesn't do css modules, so this doesn't work
+      // it('adds "md" size className', () => {
+      //   const wrapper = shallow(<Addon size="md" />);
+      //   expect(wrapper.hasClass(style.md)).toEqual(true);
+      // });
+    });
+  });
+});
