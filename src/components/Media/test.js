9c583d9dd8398b8be936ee1d582b56ea3615e12f
Matthew Wells <mdespuits@users.noreply.github.com>
2019-10-23 09:09:32 -0500

    Correctly setup re-renders so for performance (#125)

--- src/components/Media/test.js
+++ src/components/Media/test.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { mount } from 'enzyme';
 
 import Media from './';
+
+const TestWrapper = () => {
+  const [val, setVal] = useState('');
+
+  const handleOnChange = e => {
+    setVal(e.target.value);
+  };
+
+  return (
+    <Media>
+      <Media.Body id="media-body">
+        <input
+          id="input-test"
+          type="text"
+          value={val}
+          onChange={handleOnChange}
+        />
+      </Media.Body>
+    </Media>
+  );
+};
 
 describe('Media', () => {
   describe('Media.Item', () => {
@@ -14,6 +35,18 @@ describe('Media', () => {
 
       expect(wrapper.find('main')).toHaveLength(1);
       expect(wrapper.find('main aside')).toHaveLength(1);
+    });
+
+    it('will not re-render everything unnecessarily', () => {
+      const wrapper = mount(<TestWrapper />);
+      const input = wrapper.find('input#input-test');
+
+      input.instance().focus();
+
+      input.simulate('change', { target: { value: 'h' } });
+      input.simulate('change', { target: { value: 'he' } });
+
+      expect(input.is(':focus')).toBe(true);
     });
 
     it('renders its children', () => {
