44d188747e50525e2ea32f7726b3eb83f91c52c7
Matthew Wells <wellsmattd@gmail.com>
2020-04-02 15:11:42 -0500

    More Tooltip additions and cleanup

--- src/components/Tooltip/index.js
+++ src/components/Tooltip/index.js
@@ -11,12 +11,14 @@ export const directions = ['top', 'left', 'right', 'bottom'];
 const Tooltip = ({
   children,
   closeable,
+  closeIcon,
   content: tooltipContent,
   direction,
   hover,
   tooltipOpts: tooltipOptsProp,
   open,
   onClose,
+  text,
   width,
   ...rest
 }) => {
@@ -24,15 +26,8 @@ const Tooltip = ({
   const tooltipRef = useRef(null);
 
   const [hovered, setHovered] = useState(false);
-  const [childrenWidth, setWidth] = useState({});
-  const [childrenHeight, setHeight] = useState(0);
-
-  useEffect(() => {
-    if (childrenRef.current) {
-      setHeight(childrenRef.current.offsetHeight);
-      setWidth(childrenRef.current.offsetWidth);
-    }
-  });
+  const [tooltipHeight, setTooltipHeight] = useState(0);
+  const [childrenHeight, setChildrenHeight] = useState(0);
 
   const handleSetHover = value => () => {
     if (hover) {
@@ -40,23 +35,11 @@ const Tooltip = ({
     }
   };
 
-  const offsets = useMemo(() => {
-    if (direction === 'top') {
-      return {
-        bottom: childrenHeight + 20,
-      };
-    } else if (direction === 'bottom') {
-      return {
-        top: childrenHeight + 20,
-      };
-    } else if (direction === 'right') {
-      return {
-        left: childrenWidth + 20,
-      };
+  useEffect(() => {
+    if (tooltipRef.current) {
+      setTooltipHeight(tooltipRef.current.offsetHeight);
+      setChildrenHeight(childrenRef.current.offsetHeight);
     }
-    return {
-      right: childrenWidth + 20,
-    };
   });
 
   const {
@@ -65,26 +48,52 @@ const Tooltip = ({
     ...tooltipOpts
   } = tooltipOptsProp;
 
+  const tooltipClassNames = classNames(
+    styles.Tooltip,
+    styles[direction],
+    tooltipClassName,
+    {
+      [styles.open]: open || hovered,
+    }
+  );
+
+  const offsets = useMemo(() => {
+    // Properly center tooltips on the left and right to the center
+    // of the actual element we are attaching the tooltip.
+    if (direction === 'left' || direction === 'right') {
+      return {
+        top: (childrenHeight - tooltipHeight) / 2,
+      };
+    }
+
+    return {};
+  });
+
   const tooltipProps = {
     ...tooltipOpts,
     style: {
-      ...offsets,
       width: (style && style.width) || width,
+      ...offsets,
+      ...(tooltipOpts.style || {}),
     },
-    className: classNames(styles.Tooltip, styles[direction], tooltipClassName, {
-      [styles.open]: open || hovered,
-    }),
+    className: tooltipClassNames,
   };
 
-  const TooltipContent = tooltipContent && (
-    <div {...tooltipProps}>
+  const TooltipContent = (text || tooltipContent) && (
+    <div ref={tooltipRef} {...tooltipProps}>
       <div style={style} className={styles.tooltipInnerWrapper}>
         {closeable && (
           <button className={styles.tooltipClose} onClick={onClose}>
-            <Icon name="close" />
+            <Icon name={closeIcon} />
           </button>
         )}
-        <div ref={tooltipRef}>{tooltipContent}</div>
+        <div
+          className={classNames({
+            [styles.textContent]: text,
+          })}
+        >
+          {text || tooltipContent}
+        </div>
       </div>
     </div>
   );
@@ -107,6 +116,7 @@ const Tooltip = ({
 Tooltip.propTypes = {
   children: PropTypes.node.isRequired,
   closeable: PropTypes.bool,
+  closeIcon: PropTypes.string,
   content: PropTypes.oneOfType([
     PropTypes.node,
     PropTypes.string,
@@ -127,13 +137,14 @@ Tooltip.propTypes = {
 Tooltip.defaultProps = {
   content: null,
   closeable: false,
+  closeIcon: 'close',
   direction: 'top',
   hover: false,
   onClose: () => {},
   open: false,
-  text: '',
+  text: null,
   tooltipOpts: {},
-  width: '150px',
+  width: null,
 };
 
 export default Tooltip;
--- src/components/Tooltip/story.js
+++ src/components/Tooltip/story.js
@@ -1,32 +1,39 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React, { Fragment, useState } from 'react';
 import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 
 import Tooltip from './';
 import Button from '../Button';
-import Readme from './README.md';
+
+export default {
+  title: 'Planets/Tooltip',
+  component: Tooltip,
+};
+
+const TooltipContent = (
+  <Fragment>
+    <h3 style={{ margin: 0 }}>Hallo there!</h3>
+    <p>
+      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+      ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget,
+      tempor sit amet, ante. Donec eu libero sit amet quam egestas semper.
+      Aenean
+    </p>
+  </Fragment>
+);
 
 // eslint-disable-next-line react/prop-types
-const SBTooltip = ({ children, ...rest }) => {
+const SBTooltip = ({ children, onClose, ...rest }) => {
   const [open, setOpen] = useState(false);
-
-  const TooltipContent = (
-    <Fragment>
-      <h3 style={{ margin: 0 }}>Hallo there!</h3>
-      <p>
-        Pellentesque habitant morbi tristique senectus et netus et malesuada
-        fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-        ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-        egestas semper. Aenean
-      </p>
-    </Fragment>
-  );
 
   return (
     <Tooltip
-      content={TooltipContent}
       open={open}
-      onClose={() => setOpen(false)}
+      onClose={() => {
+        setOpen(false);
+        onClose();
+      }}
       {...rest}
     >
       <Button style={{ marginRight: 10 }} onClick={() => setOpen(true)}>
@@ -36,54 +43,104 @@ const SBTooltip = ({ children, ...rest }) => {
   );
 };
 
+const FlexWrapper = props => (
+  <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignContent: 'center',
+      alignItems: 'center',
+      padding: '20px',
+      height: '50vh',
+    }}
+    {...props}
+  />
+);
+
+const Element = props => <div style={{ marginRight: 20 }} {...props} />;
+
 storiesOf('Planets/Tooltip', module)
-  .addParameters({
-    readme: {
-      sidebar: Readme,
-    },
-  })
   .add(
-    'Overview',
+    'Hoverable',
     () => {
       return (
-        <Fragment>
-          <div
-            style={{
-              display: 'flex',
-              justifyContent: 'center',
-              alignContent: 'center',
-              alignItems: 'center',
-              padding: '20px',
-              height: '50vh',
-            }}
-          >
-            <SBTooltip closeable direction="bottom" width="225px">
-              Tooltip to Bottom
-            </SBTooltip>
+        <FlexWrapper>
+          <Element>
             <Tooltip
+              style={{ marginRight: '20px' }}
+              hover
               open
-              width="160px"
-              content="hello with different with different content"
+              text="Hello, I'm on top!"
             >
-              <div style={{ marginRight: '10px' }}>Another tooltip</div>
+              <div>Hover on the top</div>
             </Tooltip>
-            <SBTooltip closeable direction="top" width="700px">
-              Tooltip to Top
-            </SBTooltip>
-            <SBTooltip closeable direction="left" width="400px">
-              Tooltip to Left
-            </SBTooltip>
-            <SBTooltip closeable direction="right" width="225px">
-              Tooltip to Right
-            </SBTooltip>
-          </div>
-        </Fragment>
+          </Element>
+          <Element>
+            <Tooltip
+              style={{ marginRight: '20px' }}
+              hover
+              direction="bottom"
+              text="...and I'm on the bottom!"
+            >
+              <div>Hover on the bottom</div>
+            </Tooltip>
+          </Element>
+          <Element>
+            <Tooltip hover direction="left" text="...and I'm on the left!">
+              <div>Hover on the left</div>
+            </Tooltip>
+          </Element>
+          <Element>
+            <Tooltip hover direction="right" text="...and I'm on the right!">
+              <div>Hover on the right</div>
+            </Tooltip>
+          </Element>
+        </FlexWrapper>
       );
     },
-    {
-      info: {
-        inline: true,
-        source: true,
-      },
-    }
+    {}
+  )
+  .add(
+    'Closeable',
+    () => (
+      <FlexWrapper>
+        <SBTooltip
+          content={TooltipContent}
+          onClose={action('closing tooltip on bottom')}
+          closeable
+          direction="bottom"
+          width="225px"
+        >
+          Tooltip to Bottom
+        </SBTooltip>
+        <SBTooltip
+          content={TooltipContent}
+          onClose={action('closing tooltip on top')}
+          closeable
+          direction="top"
+          width="700px"
+        >
+          Tooltip to Top
+        </SBTooltip>
+        <SBTooltip
+          content={TooltipContent}
+          onClose={action('closing tooltip on right')}
+          closeable
+          direction="right"
+          width="225px"
+        >
+          Tooltip to Right
+        </SBTooltip>
+        <SBTooltip
+          content={TooltipContent}
+          onClose={action('closing tooltip on left')}
+          closeable
+          direction="left"
+          width="400px"
+        >
+          Tooltip to Left
+        </SBTooltip>
+      </FlexWrapper>
+    ),
+    {}
   );
