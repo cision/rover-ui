86062a5f9919f38616c04d46f2c89034c479638b
Matthew Wells <wellsmattd@gmail.com>
2020-05-22 16:04:01 -0500

    Tailwind and Storybook Component Helpers

--- src/components/Bar/Bar.tsx
+++ src/components/Bar/Bar.tsx
@@ -1,22 +1,57 @@
-import React from 'react';
+import React, { CSSProperties } from 'react';
+import CSS from 'csstype';
 import classNames from 'classnames';
 
 import styles from './Bar.module.css';
 
-type BarProps = React.HTMLAttributes<HTMLDivElement>;
+type BarProps = React.HTMLAttributes<HTMLDivElement> & {
+  width?: string | number | null;
+};
+
 type BarType = React.FC<BarProps> & {
   Fill: React.FC<BarProps>;
 };
 
-const Bar: BarType = ({ className = '', ...props }) => (
-  <div {...props} className={classNames(styles.Bar, className)} />
-);
+const BarGenerator = (
+  name: string,
+  barClassname: string
+): React.FC<BarProps> => {
+  const GeneratedBar: React.FC<BarProps> = ({
+    className = '',
+    width: widthProp = null,
+    style: styleProp = {},
+    ...props
+  }) => {
+    let width = widthProp;
 
-const Fill: React.FC<BarProps> = ({ className = '', ...props }) => (
-  <div {...props} className={classNames(styles.Fill, className)} />
-);
+    if (widthProp) {
+      width =
+        typeof widthProp === 'number'
+          ? `${widthProp}px`
+          : (widthProp as CSS.WidthProperty<string | number>);
+    }
 
-Fill.displayName = 'Bar.Fill';
+    const style: Omit<CSSProperties, 'width'> = {
+      width,
+      ...styleProp,
+    };
+
+    return (
+      <div
+        {...props}
+        style={style}
+        className={classNames(barClassname, className)}
+      />
+    );
+  };
+
+  GeneratedBar.displayName = name;
+
+  return GeneratedBar;
+};
+
+const Fill = BarGenerator('Bar.Fill', styles.Fill);
+const Bar = BarGenerator('Bar', styles.Bar) as BarType;
 
 Bar.Fill = Fill;
 
--- src/components/Bar/story.js
+++ src/components/Bar/story.js
@@ -1,29 +1,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { number, select } from '@storybook/addon-knobs';
-import styled from 'styled-components';
-import { fontFamily, fontSize, margin, space } from 'styled-system';
+import { Wrap, Label, Spacer, Title } from '../../stories/storybook-helpers';
 
 import Bar from './Bar';
-import Paper from '../Paper';
 import BarStatReadme from './README.md';
-import withDefaultTheme from '../withDefaultTheme';
 import theme from '../../shared/theme';
-
-const Label = withDefaultTheme(styled.span(fontFamily, fontSize));
-Label.displayName = 'Label';
-Label.defaultProps = {
-  fontFamily: 'body',
-  fontSize: 1,
-};
-
-const Wrap = withDefaultTheme(styled.div(margin));
-Wrap.defaultProps = { m: 'md' };
-Wrap.displayName = 'Wrap';
-
-const Spacer = withDefaultTheme(styled.div(space));
-Spacer.defaultProps = { mb: 'md' };
-Spacer.displayName = 'Spacer';
 
 const colorOptions = {
   'river-bed': theme.colors['river-bed'],
@@ -61,64 +43,63 @@ storiesOf('Star Systems/Bar', module)
     const fill = select('Fill', colorOptions, theme.colors.blue);
 
     return (
-      <Paper style={{ marginBottom: '10px' }}>
+      <Wrap>
         <Bar style={{ backgroundColor: background }}>
           <Bar.Fill style={{ backgroundColor: fill, width: `${value}%` }} />
         </Bar>
-        <Label>Percent: {value}</Label>
-      </Paper>
+        <Label>Percent: {value}%</Label>
+      </Wrap>
     );
   })
   .add('Examples', () => (
-    <Wrap>
-      <Paper style={{ marginBottom: '10px' }}>
-        <h3>Default Bar</h3>
+    <>
+      <Wrap>
+        <Title>Default Bar</Title>
         <Bar>
-          <Bar.Fill style={{ width: '73%' }} />
+          <Bar.Fill width="73%" />
         </Bar>
-      </Paper>
+      </Wrap>
 
-      <Paper style={{ marginBottom: '10px' }}>
-        <h3>Custom Wrapper and Fill</h3>
+      <Wrap>
+        <Title>Custom Wrapper and Fill</Title>
         <Bar style={{ background: theme.colors.loblolly }}>
-          <Bar.Fill style={{ background: theme.colors.salmon, width: '50%' }} />
+          <Bar.Fill width="50%" style={{ background: theme.colors.salmon }} />
         </Bar>
-      </Paper>
+      </Wrap>
 
-      <Paper style={{ marginBottom: '10px' }}>
-        <h3>Wrapper Width</h3>
-        <Bar style={{ width: '25%' }}>
-          <Bar.Fill style={{ background: theme.colors.salmon, width: '50%' }} />
+      <Wrap>
+        <Title>Wrapper Width</Title>
+        <Bar width="25%">
+          <Bar.Fill width="50%" style={{ background: theme.colors.salmon }} />
         </Bar>
         <Spacer />
-        <Bar style={{ width: '50%' }}>
-          <Bar.Fill style={{ background: theme.colors.green, width: '50%' }} />
+        <Bar width="50%">
+          <Bar.Fill width="50%" style={{ background: theme.colors.green }} />
         </Bar>
         <Spacer />
-        <Bar style={{ width: '75%' }}>
-          <Bar.Fill style={{ background: theme.colors.blue, width: '50%' }} />
+        <Bar width="75%">
+          <Bar.Fill width="50%" style={{ background: theme.colors.blue }} />
         </Bar>
         <Spacer />
-        <Bar style={{ width: '100%' }}>
-          <Bar.Fill style={{ background: theme.colors.teal, width: '50%' }} />
+        <Bar>
+          <Bar.Fill width="50%" style={{ background: theme.colors.teal }} />
         </Bar>
-      </Paper>
+      </Wrap>
 
-      <Paper style={{ marginBottom: '10px' }}>
-        <h3>Bars with multiple colors</h3>
-        <Bar style={{ width: '100%' }}>
-          <Bar.Fill style={{ background: theme.colors.green, width: '25%' }} />
+      <Wrap>
+        <Title>Bars with multiple colors</Title>
+        <Bar>
+          <Bar.Fill width="25%" style={{ background: theme.colors.green }} />
           <Bar.Fill
-            style={{ background: theme.colors.brandColor, width: '10%' }}
+            width="10%"
+            style={{ background: theme.colors.brandColor }}
           />
-          <Bar.Fill
-            style={{ background: theme.colors.loblolly, width: '10%' }}
-          />
+          <Bar.Fill width="10%" style={{ background: theme.colors.loblolly }} />
         </Bar>
-      </Paper>
+      </Wrap>
 
-      <Paper style={{ marginBottom: '10px' }}>
-        <h3>At various fill widths...</h3>
+      <Wrap>
+        <Title>At various fill widths...</Title>
         {[0, 10, 20, 50, 99, 100].map(percent => (
           <div key={percent.toString()}>
             <Spacer />
@@ -133,6 +114,6 @@ storiesOf('Star Systems/Bar', module)
             <Label>Percent: {percent || 0}%</Label>
           </div>
         ))}
-      </Paper>
-    </Wrap>
+      </Wrap>
+    </>
   ));
