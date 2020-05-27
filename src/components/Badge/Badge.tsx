640f50a8283cc848cb26ac5603bce663558c26ba
Matthew Wells <wellsmattd@gmail.com>
2020-05-27 17:52:57 -0500

    Drop PropTypes and keep existing API

--- src/components/Badge/Badge.tsx
+++ src/components/Badge/Badge.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
 import styles from './Badge.module.css';
@@ -35,46 +34,23 @@ interface BadgeProps
   tag?: keyof Pick<JSX.IntrinsicElements, 'div' | 'span'>;
   variant?: BadgeVariant;
   round?: boolean | null;
-  isRound?: boolean | null;
 }
 
 const Badge: React.FC<BadgeProps> = ({
   className = '',
   tag: Tag = 'span',
   round = null,
-  isRound = null,
   variant = '',
   ...rest
 }) => {
-  if (isRound === null && round !== null) {
-    // eslint-disable-next-line no-console
-    console.warn(
-      "<Badge> prop 'round' is deprecated. Prefer to use 'isRound' in the same way."
-    );
-  }
-
   const classes = classNames(
     className,
     styles.Badge,
-    { [styles.round]: isRound ?? round },
+    { [styles.round]: round },
     buildVariantClasses(variants, variant)
   );
 
   return <Tag {...rest} className={classes} />;
-};
-
-Badge.defaultProps = {
-  className: '',
-  round: null,
-  isRound: null,
-  variant: '',
-};
-
-Badge.propTypes = {
-  className: PropTypes.string,
-  round: PropTypes.bool,
-  isRound: PropTypes.bool,
-  variant: PropTypes.oneOf<BadgeVariant>(variants),
 };
 
 export default Badge;
