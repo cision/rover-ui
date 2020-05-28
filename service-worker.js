7f2d22cc1438c25556e4a376293c1cc187b1927c
mdespuits <mdespuits@users.noreply.github.com>
2020-05-28 18:48:22 +0000

    Storybook 🚀: Triggered from 212212ca8e4568bc24023f81ec330e313b199a0d 212212ca8e4568bc24023f81ec330e313b199a0d

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f9ce9c436fc6080bfcc3a9e8e26e6f8d.js"
+  "precache-manifest.3c693d2d9e01a5013ee371374ae9497a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
