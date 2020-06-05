b2adde5665761f76c9a45b1ed156e53b07722900
Matthew Wells <wellsmattd@gmail.com>
2020-06-05 15:46:45 -0500

    Beautify Input stories and cleanup CSS a bit

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -4,7 +4,8 @@ import { ClassValue } from 'classnames/types';
 
 import styles from './Input.module.css';
 
-interface InputProps {
+interface InputProps
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'className'> {
   className?: ClassValue;
   /** Applies disabled appearance without disabling. Helpful for adding tooltips on disabled field clicks. */
   fauxDisabled?: boolean;
