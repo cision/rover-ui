561a5baaa1417e02b4fe3617cbb0bcd2f98051ce
dkordik <dkordik@users.noreply.github.com>
2020-07-21 21:38:52 +0000

    Storybook 🚀: Triggered from 564637baf849e0a4f98269b27e0e61dfd72fb801 564637baf849e0a4f98269b27e0e61dfd72fb801

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.0d59e4273dd87e5b2e5390ea05489cdc.js"
+  "precache-manifest.3a2c51ffef0e520915b8d78a3ac9a538.js"
 );
 
 self.addEventListener('message', (event) => {
