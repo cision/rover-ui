937f5060cb7c27f063ebe96c31e644be002fa411
pixelbandito <pixelbandito@users.noreply.github.com>
2020-10-27 17:13:48 +0000

    Storybook 🚀: Triggered from e11b904ada1776434be91f330ddef3f331b57737 e11b904ada1776434be91f330ddef3f331b57737

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.f160be194ef2db9882e9a1066630c868.js"
+  "precache-manifest.7a4289ebdd65e04edfa406ce0126baf6.js"
 );
 
 self.addEventListener('message', (event) => {
