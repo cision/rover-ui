053a38dce25e24c96d9118e09cc31350ca806d8b
pixelbandito <pixelbandito@users.noreply.github.com>
2021-08-02 15:35:55 +0000

    Storybook 🚀: Triggered from 8c0b4d1581412384d296133adb998b0bd8bbef06 8c0b4d1581412384d296133adb998b0bd8bbef06

---
+++ precache-manifest.9c3c43c8f24469e882e9f916e609cbac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fb8a55ad0b69233b4de37c26955ea74",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.99df2035aa5978e7d797.bundle.js"
+  },
+  {
+    "url": "runtime~main.99df2035aa5978e7d797.bundle.js"
+  },
+  {
+    "revision": "a3fff99185b996c2a7ab",
+    "url": "static/css/main.4b2dd2be.chunk.css"
+  },
+  {
+    "revision": "5ae4a044b5ce00510cf83ca02f3512bd",
+    "url": "static/media/style.5ae4a044.css"
+  },
+  {
+    "url": "vendors~main.99df2035aa5978e7d797.bundle.js"
+  },
+  {
+    "url": "vendors~main.99df2035aa5978e7d797.bundle.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.da8f1f87eaf23de1bd96a101945b759d.js"
+  "precache-manifest.9c3c43c8f24469e882e9f916e609cbac.js"
 );
 
 self.addEventListener('message', (event) => {
